09b477397d789a0837d0f6080e243fcafd90769a
Freeflyjunkie <erictorres56@gmail.com>
2014-02-01 01:27:44 -0500

    More Home Page Work

--- Saleular/Controllers/HomeController.cs
+++ Saleular/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Saleular.Controllers
         public ActionResult Index()
         {
             OffersPaid offers = new OffersPaid();            
-            offers.iPhone5Ss  = db.Phones.Where(phone => phone.Type == "iPhone" && phone.Model == "5S").OrderBy(phone => phone.PhoneID).Take(10);
+            offers.iPhone5Ss  = db.Phones.Where(phone => phone.Type == "iPhone" && phone.Model == "5S").OrderBy(phone => phone.PhoneID).Take(5);
             offers.iPhone5Cs = db.Phones.Where(phone => phone.Type == "iPhone" && phone.Model == "5C");
             offers.iPhone5s = db.Phones.Where(phone => phone.Type == "iPhone" && phone.Model == "5");
             offers.iPhone4Ss = db.Phones.Where(phone => phone.Type == "iPhone" && phone.Model == "4S");         
