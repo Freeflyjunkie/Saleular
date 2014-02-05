41c114185b433a81e1f57abb9a7978190f6f4a18
Freeflyjunkie <erictorres56@gmail.com>
2014-02-04 22:46:19 -0500

    Offer Working
    Offer Working

--- Saleular/Controllers/PhoneController.cs
+++ Saleular/Controllers/PhoneController.cs
@@ -39,32 +39,15 @@ namespace Saleular.Controllers
                 selectedIPhone.Carriers = db.Phones
                     .Where(p => p.Model == model)
                     .Select(c => c.Carrier).Distinct().ToList();
+              
+                selectedIPhone.Capacities = db.Phones
+                    .Where(p => p.Model == model)
+                    .Select(c => c.Capacity).Distinct().ToList();
             }
 
             if(!selectedIPhone.Carriers.Contains(selectedIPhone.SelectedCarrier))
             {
                 selectedIPhone.SelectedCarrier = "Select Carrier...";
-            }
-
-            Session["SelectIPhone"] = selectedIPhone;
-
-            return View("Offer", selectedIPhone);
-        }
-
-        public ActionResult SelectCarrier(string carrier)
-        {
-            SelectediPhone selectedIPhone = (SelectediPhone)Session["SelectIPhone"];
-            
-            if (selectedIPhone.SelectedCarrier.Contains("Select"))
-            {
-                selectedIPhone.SelectedCarrier = carrier;
-            }
-
-            using (SaleularContext db = new SaleularContext())
-            {                
-                selectedIPhone.Capacities = db.Phones
-                    .Where(p => p.Model == selectedIPhone.SelectedModel && p.Carrier == selectedIPhone.SelectedCarrier)
-                    .Select(c => c.Capacity).Distinct().ToList();
             }
 
             if (!selectedIPhone.Capacities.Contains(selectedIPhone.SelectedCapacity))
@@ -73,7 +56,38 @@ namespace Saleular.Controllers
             }
 
             Session["SelectIPhone"] = selectedIPhone;
+            GetOffer();
+            return View("Offer", selectedIPhone);
+        }
 
+        public ActionResult SelectCarrier(string carrier)
+        {
+            SelectediPhone selectedIPhone = (SelectediPhone)Session["SelectIPhone"];
+            selectedIPhone.SelectedCarrier = carrier;            
+
+            using (SaleularContext db = new SaleularContext())
+            {
+                selectedIPhone.Carriers = db.Phones
+                    .Where(p => p.Model == selectedIPhone.SelectedModel)
+                    .Select(c => c.Carrier).Distinct().ToList();
+
+                selectedIPhone.Capacities = db.Phones
+                    .Where(p => p.Model == selectedIPhone.SelectedModel)
+                    .Select(c => c.Capacity).Distinct().ToList();
+            }
+
+            if (!selectedIPhone.Carriers.Contains(selectedIPhone.SelectedCarrier))
+            {
+                selectedIPhone.SelectedCarrier = "Select Carrier...";
+            }
+
+            if (!selectedIPhone.Capacities.Contains(selectedIPhone.SelectedCapacity))
+            {
+                selectedIPhone.SelectedCapacity = "Select Capacity...";
+            }
+
+            Session["SelectIPhone"] = selectedIPhone;
+            GetOffer();
             return View("Offer", selectedIPhone);
         }
 
@@ -81,38 +95,44 @@ namespace Saleular.Controllers
         {
             SelectediPhone selectedIPhone = (SelectediPhone)Session["SelectIPhone"];
             selectedIPhone.SelectedCapacity = capacity;
-
-            using (SaleularContext db = new SaleularContext())
-            {
-                selectedIPhone.Conditions = db.Phones
-                    .Where(p => p.Model == selectedIPhone.SelectedModel && p.Carrier == selectedIPhone.SelectedCarrier && p.Capacity == selectedIPhone.SelectedCapacity)
-                    .Select(c => c.Condition).Distinct().ToList();
-            }
-
+            selectedIPhone.Conditions = new List<string> { "Flawless", "Good", "Bad" };            
             Session["SelectIPhone"] = selectedIPhone;
-
+            GetOffer();
             return View("Offer", selectedIPhone);
         }
 
         public ActionResult SelectCondition(string condition)
         {
             SelectediPhone selectedIPhone = (SelectediPhone)Session["SelectIPhone"];
-            selectedIPhone.SelectedCondition = condition;
-
-            using (SaleularContext db = new SaleularContext())
-            {
-                selectedIPhone.Price = db.Phones
-                    .Where(p => p.Model == selectedIPhone.SelectedModel 
-                        && p.Carrier == selectedIPhone.SelectedCarrier 
-                        && p.Capacity == selectedIPhone.SelectedCapacity
-                        && p.Condition == selectedIPhone.SelectedCondition)
-                    .Select(c => c.Price).SingleOrDefault();
-            }
-
+            selectedIPhone.SelectedCondition = condition;            
             Session["SelectIPhone"] = selectedIPhone;
-
+            GetOffer();
             return View("Offer", selectedIPhone);
         }                
+
+        public void GetOffer()
+        {
+            SelectediPhone selectedIPhone = (SelectediPhone)Session["SelectIPhone"];
+            if (selectedIPhone.Models.Contains("Select")
+                || selectedIPhone.Carriers.Contains("Select")
+                || selectedIPhone.Capacities.Contains("Select")
+                || selectedIPhone.Conditions.Contains("Select"))
+            {
+                selectedIPhone.Price = 0.00M;
+            }
+            else
+            {
+                using (SaleularContext db = new SaleularContext())
+                {
+                    selectedIPhone.Price = db.Phones
+                        .Where(p => p.Model == selectedIPhone.SelectedModel
+                            && p.Carrier == selectedIPhone.SelectedCarrier
+                            && p.Capacity == selectedIPhone.SelectedCapacity
+                            && p.Condition == selectedIPhone.SelectedCondition)
+                        .Select(c => c.Price).SingleOrDefault();
+                }
+            }
+        }
 
         public ActionResult Ship()
         {
