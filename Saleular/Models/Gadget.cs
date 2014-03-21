59bb79bcf4fec68bfa0e6e59e857b879b3b79287
Freeflyjunkie <erictorres56@gmail.com>
2014-03-20 23:08:09 -0400

    Editor Templates

--- Saleular/Models/Gadget.cs
+++ Saleular/Models/Gadget.cs
@@ -10,26 +10,34 @@ namespace Saleular.Models
     public class Gadget
     {
         [Key]
+        [Required]
         public Int32 GadgetId { get; set; }
 
+        [Required]
         [StringLength(25, MinimumLength = 1)]
         public String Type { get; set; }
 
+        [Required]
         [StringLength(10, MinimumLength = 1)]
         public String Model { get; set; }
-        
+
+        [Required]
         public String Capacity { get; set; }
 
+        [Required]
         public String Carrier { get; set; }
 
+        [Required]
         [StringLength(10, MinimumLength = 1)]
         public String Condition { get; set; }
 
+        [Required]
         [StringLength(500, MinimumLength = 1)]
         public String ImageUrl { get; set; }
 
         [DataType(DataType.Currency)]
         [Column(TypeName = "money")]
+        [Required]
         public decimal Price { get; set; }
     }
 }
