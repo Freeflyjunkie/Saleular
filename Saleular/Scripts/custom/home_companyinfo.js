639767f81bb443849989bd2c8ca0f151c1598313
Freeflyjunkie <erictorres56@gmail.com>
2015-03-08 00:20:17 -0500

    ...

--- Saleular/Scripts/custom/home_companyinfo.js
+++ Saleular/Scripts/custom/home_companyinfo.js
@@ -1,14 +1,41 @@
-﻿$(document).ready(function () {    
-    var salesRep = $('#salesRep');
+﻿$(document).ready(function () {
+
+    var salesRep = $('.sales-rep-img');
     salesRep.on('mouseover', function () {
         var salesRepText = $('.sales-rep-text');
 
         salesRepText.animate({
-            opacity: .2
+            opacity: .5
         }, 100, 'swing');
 
         salesRepText.animate({
             opacity: 1
         }, 1000, 'swing');
-    });       
+    });
+
+    var iphoneMavens = $('.iphone-img-text');
+    iphoneMavens.on('mouseover', function () {
+        var iPhoneText = $('#iphoneText');
+
+        iPhoneText.animate({
+            opacity: .5
+        }, 100, 'swing');
+
+        iPhoneText.animate({
+            opacity: 1
+        }, 1000, 'swing');
+    });
+
+    var bridge = $('#nl-img-text');
+    bridge.on('mouseover', function () {
+        var nlTextId = $('#nlText');        
+
+        nlTextId.animate({
+            opacity: .5
+        }, 100, 'swing');        
+
+        nlTextId.animate({
+            opacity: 1
+        }, 1000, 'swing');        
+    });
 });
