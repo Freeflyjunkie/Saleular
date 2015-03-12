bd36b79b43c41c60e7c8ec842a6cfe7c1aa37bb5
erictorres@gmail.com <erictorres56@gmail.com>
2015-03-12 17:14:48 -0400

    ...

--- Saleular/Scripts/custom/home_companyinfo.js
+++ Saleular/Scripts/custom/home_companyinfo.js
@@ -1,15 +1,59 @@
 ﻿$(document).ready(function () {
     
-    $('#salesRep').hover(function () {
-        $('#salesRepText').toggleClass("ci-text-shadow");
+    var c = $('#carousel');
+    c.carousel();
+
+    swingInCustomerServiceText();
+
+    c.on('slid', function () {
+        var index = $(this).find('.active').index();
+        
+        if (index == 0) {
+            
+        }
+
+        if (index == 1) {
+            swingInCustomerServiceText();
+        }
     });
 
-    $('#iphones').hover(function () {        
-        $('#iphoneText').toggleClass("ci-text-shadow");
-    });
+    function swingInCustomerServiceText()
+    {
+        var customerService = $('#customerServiceText');
+        customerService.animate({
+            opacity: 1,
+            left: '35px'
+        }, 1000, 'swing');
 
-    $('#location').hover(function () {
-        $('#locationText').toggleClass("ci-text-shadow");
-        $('#bridgeText').toggleClass("ci-text-shadow-white");
-    });
+        var testimonialsTextHeader = $('#testimonialsTextHeader');
+        testimonialsTextHeader.animate({
+            opacity: 1,
+            left: '35px'
+        }, 2000, 'swing');
+
+        var testimonialsText1 = $('#testimonialsText1');
+        testimonialsText1.animate({
+            opacity: 1,
+            left: '35px'
+        }, 3000, 'swing');
+
+        var testimonialsText2 = $('#testimonialsText2');
+        testimonialsText2.animate({
+            opacity: 1,
+            left: '145px'
+        }, 3000, 'swing');
+    }
+
+    //$('#salesRep').hover(function () {
+    //    $('#salesRepText').toggleClass("ci-text-shadow");
+    //});
+
+    //$('#iphones').hover(function () {        
+    //    $('#iphoneText').toggleClass("ci-text-shadow");
+    //});
+
+    //$('#location').hover(function () {
+    //    $('#locationText').toggleClass("ci-text-shadow");
+    //    $('#bridgeText').toggleClass("ci-text-shadow-white");
+    //});
 });
