1f8e98993f5c8c8310dde539f4081c1451a259f7
Freeflyjunkie <erictorres56@gmail.com>
2015-03-12 00:08:56 -0400

    enhancements

---
+++ Saleular/Scripts/custom/home_home.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    var caption = $('.iphone-caption');
+    var caption2 = $('.iphone-caption2');
+    var caption3 = $('.iphone-caption3');
+
+    caption.animate({
+        opacity: 1,
+        top: 20
+    }, 3000, 'swing');
+
+    var interval = setInterval(function () {
+        caption2.animate({
+            opacity: 1
+        }, 1000, 'swing');
+    }, 3000);
+
+    var interval2 = setInterval(function () {
+        caption3.animate({
+            opacity: 1
+        }, 1000, 'swing');
+    }, 4000);    
+});
