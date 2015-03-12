bcc6941b4db678be8800024711502a107c22bf92
erictorres@gmail.com <erictorres56@gmail.com>
2015-03-12 13:59:25 -0400

    ...

--- Saleular/Scripts/custom/home_home.js
+++ Saleular/Scripts/custom/home_home.js
@@ -5,18 +5,14 @@
 
     caption.animate({
         opacity: 1,
-        top: 20
+        top: '20px'
+    }, 1000, 'swing');
+
+    caption2.animate({
+        opacity: 1
     }, 3000, 'swing');
 
-    var interval = setInterval(function () {
-        caption2.animate({
-            opacity: 1
-        }, 1000, 'swing');
-    }, 3000);
-
-    var interval2 = setInterval(function () {
-        caption3.animate({
-            opacity: 1
-        }, 1000, 'swing');
-    }, 4000);    
+    caption3.animate({
+        opacity: 1
+    }, 4000, 'swing');   
 });
