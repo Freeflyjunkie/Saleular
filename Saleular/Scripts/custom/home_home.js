8ea732fbca028a69f98b6f60bb840f1ae1410915
erictorres@gmail.com <erictorres56@gmail.com>
2015-03-16 17:14:11 -0400

    enhancements

--- Saleular/Scripts/custom/home_home.js
+++ Saleular/Scripts/custom/home_home.js
@@ -14,5 +14,27 @@
 
     caption3.animate({
         opacity: 1
-    }, 3000, 'swing');   
+    }, 3000, 'swing');
+
+    var twitterLink = $('#twitterLink');
+    var msLink = $('#msLink');
+    var fbLink = $('#fbLink');
+
+    twitterLink.on('click', function() {
+        var twitterWindow = window.open('http://www.twitter.com/saleular', 'twitterWindow');
+        twitterWindow.focus();
+        return false;
+    });
+
+    msLink.on('click', function () {
+        var msWindow = window.open('http://www.twitter.com/saleular', 'msWindow');
+        msWindow.focus();
+        return false;
+    });
+
+    fbLink.on('click', function () {
+        var fbWindow = window.open('http://www.twitter.com/saleular', 'fbWindow');
+        fbWindow.focus();
+        return false;
+    });
 });
